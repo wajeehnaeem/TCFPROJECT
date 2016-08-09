a762250f8fb1ea7db06815976c814e4ef02b78ff
arsalanWahid <arsalanwahid1993@gmail.com>
2016-08-09 06:03:09 -0700

    added a new controller arsalan

---
+++ TCFPROJECT/Controllers/RandomController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCFPROJECT.Controllers
+{
+    public class RandomController : Controller
+    {
+    }
+}
